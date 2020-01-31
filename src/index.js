4126c1abd56f5db71d0115e63cb2fee717cbb5c2
Gabriel R <gabriel.ribeiro@argo-int.com>
2020-01-31 11:43:28 -0300

    update dependencies to npm

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import { ThemeProvider } from 'styled-components'
+
 export * from './components'
+export { default as GlobalStyles } from './GlobalStyles'
+export { default as theme } from './config/theme'
+export { ThemeProvider as QuanticThemeProvider }
