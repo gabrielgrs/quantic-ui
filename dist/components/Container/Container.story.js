eac55912d9e0fa0a20c3c0b2f2b2b7aabf2a5c86
Gabriel R <gabriel.ribeiro@argo-int.com>
2020-02-08 22:22:33 -0300

    remove dist

--- dist/components/Container/Container.story.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Container } from '..';
-export default {
-  title: 'Grid/Container'
-};
-export var Base = function Base() {
-  return React.createElement(Container, null, "Container");
-};
